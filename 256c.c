9271958a7a23e29e5e6f79079e9b18b680d5cf1f
Yamato <140510316+YamatoSusumu@users.noreply.github.com>
2023-07-25 14:09:47 +0200

    Update 256colors.c

--- 256c.c
+++ 256c.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main(){
+int main(void){
     int i;
-    char esc[] = "\\x1b[48;5;";
     for (i = 0; i != 257; i++)
-        printf("\x1b[48;5;%dm   %s%dm   \x1b[0m", i, esc, i);
+        printf("\x1b[48;5;%dm  \\x1b[48;5;%dm   \x1b[0m", i, i);
     printf("\n");
     return 0;
 }
